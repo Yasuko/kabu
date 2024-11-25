8ecef44e24ab185e82fe15ddcf5e03c741cf98b7
moriya <ton_ma@hotmail.com>
2024-11-26 07:55:46 +0900

    [add] コメント追加

--- model/schema/Financials.py
+++ model/schema/Financials.py
@@ -43,33 +43,33 @@ class FinancialsType:
     TaxProvision: float             # 税金負担
     PretaxIncome: float             # 税引き前利益
     OtherNonOperatingIncomeExpenses: float  # その他の非営業収益費用
-    SpecialIncomeCharges: float
-    OtherSpecialCharges: float
-    WriteOff: float
-    NetNonOperatingInterestIncomeExpense: float
-    InterestExpenseNonOperating: float
-    InterestIncomeNonOperating: float
-    OperatingIncome: float
-    OperatingExpense: float
-    OtherOperatingExpenses: float
-    SellingGeneralAndAdministration: float
-    SellingAndMarketingExpense: float
-    GeneralAndAdministrativeExpense: float
-    GrossProfit: float
-    CostOfRevenue: float
-    TotalRevenue: float
-    OperatingRevenue: float
-    RentExpenseSupplemental: float
-    RentAndLandingFees: float
-    RestructuringAndMergernAcquisition: float
-    DepreciationAndAmortizationInIncomeStatement: float
-    DepreciationIncomeStatement: float
-    DepreciationAmortizationDepletionIncomeStatement: float
-    ImpairmentOfCapitalAssets: float
-    ResearchAndDevelopment: float
-    TotalOtherFinanceCost: float
-    Amortization: float
-    AmortizationOfIntangiblesIncomeStatement: float
+    SpecialIncomeCharges: float     # 特別収益費用
+    OtherSpecialCharges: float      # その他の特別費用
+    WriteOff: float                 # 償却
+    NetNonOperatingInterestIncomeExpense: float # 非営業利息収益費用
+    InterestExpenseNonOperating: float  # 非営業利息費用
+    InterestIncomeNonOperating: float   # 非営業利息収益
+    OperatingIncome: float          # 営業利益
+    OperatingExpense: float         # 営業費用
+    OtherOperatingExpenses: float   # その他の営業費用
+    SellingGeneralAndAdministration: float      # 販売一般管理費
+    SellingAndMarketingExpense: float           # 販売マーケティング費用
+    GeneralAndAdministrativeExpense: float      # 一般管理費
+    GrossProfit: float              # 粗利益
+    CostOfRevenue: float            # 売上原価
+    TotalRevenue: float             # 売上高
+    OperatingRevenue: float         # 営業収益
+    RentExpenseSupplemental: float  # 補足賃貸費用
+    RentAndLandingFees: float       # 賃貸着陸料
+    RestructuringAndMergernAcquisition: float   # 再編成と合併費用
+    DepreciationAndAmortizationInIncomeStatement: float # 減価償却費
+    DepreciationIncomeStatement: float          # 減価償却費
+    DepreciationAmortizationDepletionIncomeStatement: float # 減価償却費
+    ImpairmentOfCapitalAssets: float            # 資本資産の減損
+    ResearchAndDevelopment: float           # 研究開発
+    TotalOtherFinanceCost: float            # その他の財務費用
+    Amortization: float                     # 減価償却費
+    AmortizationOfIntangiblesIncomeStatement: float # 無形資産の減価償却費
     OtherIncomeExpense: float
     GainOnSaleOfPpe: float
     GainOnSaleOfBusiness: float
@@ -200,3 +200,80 @@ CREATE INDEX ON financials (companyCode);
             print(e)
             exit()
 
+
+
+'''
+1. TaxEffectOfUnusualItems - 特殊項目の税効果
+2. TaxRateForCalcs - 計算用税率
+3. NormalizedEBITDA - 正常化後EBITDA
+4. TotalUnusualItems - 特殊項目の合計
+5. TotalUnusualItemsExcludingGoodwill - のれんを除く特殊項目の合計
+6. NetIncomeFromContinuingOperationNetMinorityInterest - 少数株主持分を除く継続事業からの純利益
+7. ReconciledDepreciation - 修正後減価償却費
+8. ReconciledCostOfRevenue - 修正後売上原価
+9. EBITDA - EBITDA（利息・税金・償却前利益）
+10. EBIT - EBIT（利息・税金控除前利益）
+11. NetInterestIncome - 純利息収入
+12. InterestExpense - 利息費用
+13. OtherNonInterestExpense - その他の無利息費用
+14. InterestIncome - 利息収入
+15. NormalizedIncome - 正常化後純利益
+16. NetIncomeFromContinuingAndDiscontinuedOperation - 継続および中止事業からの純利益
+17. TotalExpenses - 総費用
+18. TotalOperatingIncomeAsReported - 報告されている営業利益総計
+19. DilutedAverageShares - 希薄化後平均株式数
+20. BasicAverageShares - 基本平均株式数
+21. DilutedEPS - 希薄化後EPS
+22. BasicEPS - 基本EPS
+23. DilutedNIAvailtoComStockholders - コモン株主に帰属する希薄化後純利益
+24. NetIncomeCommonStockholders - コモン株主に帰属する純利益
+25. OtherunderPreferredStockDividend - 優先株配当の下のその他
+26. NetIncome - 純利益
+27. MinorityInterests - 少数株主持分
+28. NetIncomeIncludingNoncontrollingInterests - 非支配持分を含む純利益
+29. NetIncomeContinuousOperations - 継続事業からの純利益
+30. NetIncomeDiscontinuousOperations - 中止事業からの純利益
+31. NetIncomeFromTaxLossCarryforward - 繰越税損からの純利益
+32. TaxProvision - 税引当金
+33. PretaxIncome - 税引前利益
+34. OtherNonOperatingIncomeExpenses - その他の営業外損益
+35. SpecialIncomeCharges - 特別収益費用
+36. OtherSpecialCharges - その他の特別費用
+37. WriteOff - 減損
+38. NetNonOperatingInterestIncomeExpense - 純営業外利息損益
+39. InterestExpenseNonOperating - 営業外利息費用
+40. InterestIncomeNonOperating - 営業外利息収入
+41. OperatingIncome - 営業利益
+42. OperatingExpense - 営業費用
+43. OtherOperatingExpenses - その他営業費用
+44. SellingGeneralAndAdministration - 販売費及び一般管理費
+45. SellingAndMarketingExpense - 販売及びマーケティング費用
+46. GeneralAndAdministrativeExpense - 一般及び管理費用
+47. GrossProfit - 粗利益
+48. CostOfRevenue - 売上原価
+49. TotalRevenue - 総収入
+50. OperatingRevenue - 営業収入
+51. RentExpenseSupplemental - 補足賃料費用
+52. RentAndLandingFees - 賃料及び着陸料
+53. RestructuringAndMergernAcquisition - リストラクチャリング及び合併・買収
+54. DepreciationAndAmortizationInIncomeStatement - 損益計算書における減価償却及び償却費
+55. DepreciationIncomeStatement - 損益計算書の減価償却費
+56. DepreciationAmortizationDepletionIncomeStatement - 損益計算書の減価償却・償却・枯渇費
+57. ImpairmentOfCapitalAssets - 資本資産の減損
+58. ResearchAndDevelopment - 研究開発費
+59. TotalOtherFinanceCost - その他の金融費用総計
+60. Amortization - 償却
+61. AmortizationOfIntangiblesIncomeStatement - 損益計算書における無形資産の償却
+62. OtherIncomeExpense - その他収益費用
+63. GainOnSaleOfPpe - 有形固定資産売却益
+64. GainOnSaleOfBusiness - 事業売却益
+65. GainOnSaleOfSecurity - 証券売却益
+66. EarningsFromEquityInterest - 持分利益
+67. EarningsFromEquityInterestNetOfTax - 税引後持分利益
+68. AverageDilutionEarnings - 希薄化平均利益
+69. OtherGandA - その他の一般及び管理費
+70. NetPolicyholderBenefitsAndClaims - 保険契約者利益及びクレームの純額
+71. InsuranceAndClaims - 保険及びクレーム
+72. SalariesAndWages - 給与及び賃金
+73. OccupancyAndEquipment - 占有及び設備費
+'''
